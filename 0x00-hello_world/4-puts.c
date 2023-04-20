e76843c8e1fd8312b2a84072385baa005a7596ae
Felistus <kiokofpm@gmail.com>
2023-04-20 14:50:31 -0700

    Hello, puts commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * program to print a statement using puts
+ *
+ * return a value 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
