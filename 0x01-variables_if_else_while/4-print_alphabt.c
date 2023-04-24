590fae439f31a13e9bdc9b9af021ec5480a8b5eb
Felistus <kiokofpm@gmail.com>
2023-04-24 16:06:36 -0700

    4-print_alphabt.c commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Printing in lowercase,
+ * exceptional of some characters
+ *
+ * Return: value 0
+ */
+
+int main(void)
+{
+	int a;
+	char alp[24] = "abcdfghijklmnoprstuvwxyz";
+
+	for (a = o; a < 24; a++)
+	{
+		putchar(alp[a]);
+	}
+	putchar('\n');
+	return (0);
+}
