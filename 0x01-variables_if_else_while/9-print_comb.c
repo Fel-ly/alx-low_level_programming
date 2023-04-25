e79b3b13067c58f635045c54021a3372c9a0d266
Felistus <kiokofpm@gmail.com>
2023-04-24 17:37:22 -0700

    9-print_comb.c commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: value 0
+ *
+ */
+
+int main(void)
+{
+
+	int a;
+
+	for (a = 0; a < 10, a++)
+	{
+		putchar('0' + a);
+
+		if (a != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
