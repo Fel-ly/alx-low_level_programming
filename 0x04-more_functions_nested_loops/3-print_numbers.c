3cf2f14b3fbaac3a4866bd1f4e07a72c5c29f05d
Felistus <kiokofpm@gmail.com>
2023-04-27 14:56:03 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: val 0
+ */
+
+void print_numbers(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+
+}
