d17363a30900cacdd6e7d4a10be33416f3a88aff
Felistus <kiokofpm@gmail.com>
2023-04-27 13:11:50 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (a > b && c > a)
-	{
-		largest = c;
-	}
-	else (b > c)
+	else if (b > c)
 	{
 		largest = b;
+	}
+	else		
+	{
+		largest = c;
 	}
 
 	return (largest);
