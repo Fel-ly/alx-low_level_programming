d6401f8db7b5d97aaf07024197ce0f9e66f59cd9
Felistus <kiokofpm@gmail.com>
2023-04-27 16:30:05 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible different combinations of 2 digits
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 90; i++)
+	{
+		if(i/10 < i % 10)
+		{
+			putchar('0' + i / 10);
+			putchar('0' + i % 10);
+
+			if (i != 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
