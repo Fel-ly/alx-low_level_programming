e35669410926029a67f6acb2b00f743e2ac5fe3c
Felistus <kiokofpm@gmail.com>
2023-05-12 13:56:24 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed into it
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: int value
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%d\n", --argc);
+	return (0);
+}
