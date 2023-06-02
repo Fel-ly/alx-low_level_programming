452861824d9479b22b3150785f14bb2e549df935
Felistus <kiokofpm@gmail.com>
2023-06-02 15:34:46 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,7 +4,7 @@
 /**
  * print_list - prints all elements of a list list_t
  *
- * @k: pointer to list_t
+ * @h: pointer to list_t
  *
  * Return: number of nodes
  */
@@ -18,7 +18,7 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
